6b9e76d6c8434debe3e36919c3d5b2e6223ea38e
adalbert01997 <adalbert01997@hotmail.com>
2024-01-13 20:53:06 -0600

    feat: Added get_city_data function

--- src/constants.py
+++ src/constants.py
@@ -1,11 +1,14 @@
 import os
 
-SCHOOLS_API_PAGE_LIMIT=2
+SCHOOLS_API_PAGE_LIMIT = 1
 SCHOOLS_ENDPOINT = os.environ.get("SCHOOLS_ENDPOINT")
 SCHOOLS_API_KEY = os.environ.get("SCHOOLS_API_KEY")
 
-UNIVERSITY_PARAMS ={
-    'api_key': SCHOOLS_API_KEY,
-    'fields': 'id,school.name,school.city,school.state,school.zip,school.school_url,school.ownership,school.main_campus,school.online_only,school.open_admissions_policy,school.degrees_awarded.predominant,school.degrees_awarded.predominant_recoded,',  # Número de resultados por página
-    'page': 0
+UNIVERSITY_PARAMS = {
+    "api_key": SCHOOLS_API_KEY,
+    "fields": "id,school.name,school.city,school.state,school.zip,school.school_url,school.ownership,school.main_campus,school.online_only,school.open_admissions_policy,school.degrees_awarded.predominant,school.degrees_awarded.predominant_recoded,",  # Número de resultados por página
+    "page": 0,
 }
+
+CITIES_API_ENDPOINT = os.environ.get("CITIES_API_ENDPOINT")
+CITIES_API_KEY = os.environ.get('CITIES_API_KEY')
--- src/get_apis_data.py
+++ src/get_apis_data.py
@@ -7,12 +7,29 @@ Created on Wed Jan 10 14:53:48 2024
 
 # Python Libraries
 import os
+import json
 import requests
 import pandas as pd
 from math import floor
 
 # Python Modules
 import constants as C
+
+
+def get_city_data(cities):
+    city_data = []
+    for city in cities:
+        api_url = f'{C.CITIES_API_ENDPOINT}?name={city}' 
+        response = requests.get(api_url, headers={'X-Api-Key': C.CITIES_API_KEY})
+        if response.status_code == requests.codes.ok:
+            print(response.text)
+            if len(json.loads(response.text)) > 0:
+                city_data.append(json.loads(response.text)[0])
+        else:
+            print("Error:", response.status_code, response.text)
+    df = pd.DataFrame(city_data)
+    pd.set_option("display.max_columns", None)
+    return df
 
 def get_school_data(params, page_limit, url):
     # Inicializar una lista para almacenar los resultados
@@ -24,32 +41,40 @@ def get_school_data(params, page_limit, url):
         if response.status_code == 200:
             # Convertir la respuesta a formato JSON
             data = response.json()
-            max_page = floor(int(data['metadata']['total']) / int(data['metadata']['per_page']))
-            if int(data['metadata']['page']) > max_page or data['metadata']['page'] == page_limit:
+            max_page = floor(
+                int(data["metadata"]["total"]) / int(data["metadata"]["per_page"])
+            )
+            if (
+                int(data["metadata"]["page"]) > max_page
+                or data["metadata"]["page"] == page_limit
+            ):
                 break
-            print(params['page'])
-            params['page'] += 1
+            print(params["page"])
+            params["page"] += 1
             # print(data)
             # print(data['metadata']['page'])
 
             # Iterar sobre los resultados y extraer la información relevante
-            for result in data['results']:
+            for result in data["results"]:
                 # Crear un diccionario para almacenar la información de esta fila
                 row = {
-                    'id': result['id'],
-                    'name': result['school.name'],
-                    'city': result['school.city'],
-                    'state': result['school.state'],
-                    'zip': result['school.zip'],
-                    'url': result['school.school_url'],
-                    'ownership': result['school.ownership'],
-                    'main_campus': result['school.main_campus'],
-                    'online_only': result['school.online_only'],
-                    'open_admissions_policy': result['school.open_admissions_policy'],
-                    'degrees_awarded_predominant':result['school.degrees_awarded.predominant'],
-                    'degrees_awarded_predominant_recoded':result['school.degrees_awarded.predominant_recoded'],
+                    "id": result["id"],
+                    "name": result["school.name"],
+                    "city": result["school.city"],
+                    "state": result["school.state"],
+                    "zip": result["school.zip"],
+                    "url": result["school.school_url"],
+                    "ownership": result["school.ownership"],
+                    "main_campus": result["school.main_campus"],
+                    "online_only": result["school.online_only"],
+                    "open_admissions_policy": result["school.open_admissions_policy"],
+                    "degrees_awarded_predominant": result[
+                        "school.degrees_awarded.predominant"
+                    ],
+                    "degrees_awarded_predominant_recoded": result[
+                        "school.degrees_awarded.predominant_recoded"
+                    ],
                 }
-
 
                 # Agregar la fila a la lista de resultados
                 results_list.append(row)
@@ -59,13 +84,17 @@ def get_school_data(params, page_limit, url):
             return None
     # Crear un DataFrame a partir de la lista de resultados
     df = pd.DataFrame(results_list)
-    pd.set_option('display.max_columns', None)
+    pd.set_option("display.max_columns", None)
     # Mostrar el DataFrame
     return df
 
+
 def main():
     university_df = get_school_data(C.UNIVERSITY_PARAMS, C.SCHOOLS_API_PAGE_LIMIT, C.SCHOOLS_ENDPOINT)
-    print(university_df)
+    school_cities = set(university_df.city.tolist())
+    city_df = get_city_data(school_cities)
+    print(city_df)
+
 
 if __name__ == "__main__":
     main()
